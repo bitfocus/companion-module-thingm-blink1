6a7ce0faf6d263005c5fff4992a456078c2c4555
Jeffrey Davidsz <jeffrey.davidsz@vicreo.eu>
2019-02-03 14:39:19 +0100

    name change

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ instance.prototype.config_fields = function () {
 			id: 'info',
 			width: 12,
 			label: 'Information',
-			value: 'This module is for the Blink(1) device from ThinkM'
+			value: 'This module is for the Blink(1) device from ThingM, sending a HTTP GET'
 		},
 		{
 			type: 'textinput',
